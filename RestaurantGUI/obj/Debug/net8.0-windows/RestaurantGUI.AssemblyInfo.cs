f039ff372e6662a8f0e4adb128a47d9122b9c12f
pamod <pamod97@gmail.com>
2025-01-20 21:50:45 +0100

    sprawdzenie ściągania gui

--- RestaurantGUI/obj/Debug/net8.0-windows/RestaurantGUI.AssemblyInfo.cs
+++ RestaurantGUI/obj/Debug/net8.0-windows/RestaurantGUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestaurantGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6142fbe202befcb4cf3844c3265dc1f0ce65f3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RestaurantGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RestaurantGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
