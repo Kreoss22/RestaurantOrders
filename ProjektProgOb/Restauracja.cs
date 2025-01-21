5e51a577ceb80be06222a5916c8ff393ce4c0307
pamod <pamod97@gmail.com>
2025-01-22 00:41:02 +0100

    dodanie konstruktora do klast Restauracja

--- ProjektProgOb/Restauracja.cs
+++ ProjektProgOb/Restauracja.cs
@@ -8,13 +8,22 @@ namespace Restaurant
 {
     internal class Restauracja
     {
-        Dictionary<string, Zamowienie> zamowienia;
+        Dictionary<string, Zamowienie> zamowienia; // klucz - id zamowienia
         List<Danie> dania;
         private List<Konto> konta;
-        private List<Pracownik> pracownik;
+        private List<Pracownik> pracownicy;
         string domena;
+        string nazwa;
 
 
-        //Restauracja Dodaj konto (stworzenie klienta/pracownika)
+        public Restauracja(string domena, string nazwa)
+        {
+            this.domena = domena;
+            this.nazwa = nazwa;
+            this.pracownicy = new List<Pracownik>();
+            this.konta = new List<Konto>();
+            this.zamowienia = new Dictionary<string, Zamowienie>();
+            this.dania = new List<Danie>();
+        }
     }
 }
