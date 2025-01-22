bb15eabe250cc59d7dcc2f7a4f1b17961e5ddf53
amarek <amarek@student.agh.edu.pl>
2025-01-22 19:14:17 +0100

    okna dodaj konto/klienta/pracownika/danie

---
+++ RestaurantGUI/DodajKlienta.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestaurantGUI
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DodajKlienta.xaml
+    /// </summary>
+    public partial class DodajKlienta : Window
+    {
+        public DodajKlienta()
+        {
+            InitializeComponent();
+        }
+    }
+}
